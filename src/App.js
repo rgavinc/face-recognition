407842e1c01b26e659eaf8ffd55bf23b9a4269a2
Gavin Robertson <gavinrob92@gmail.com>
2018-12-15 12:38:43 -0700

    Added simple frontend routing for login/llogout/register/home

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import Navigation from "./Components/Navigation/Navigation"
 import Logo from "./Components/Logo/Logo"
 import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
+import Signin from "./Components/Signin/Signin";
 import Rank from "./Components/Rank/Rank";
 import './App.css';
 import Particles from 'react-particles-js';
 import Clarifai from "clarifai";
-import FaceRecognition from "./Components/FaceRecognition/FaceRecognition"
+import FaceRecognition from "./Components/FaceRecognition/FaceRecognition";
+import Register from "./Components/Register/Register";
 
 const app = new Clarifai.App({
   apiKey: 'f58db3e72129494cbe9650eb2e383c5e'
@@ -19,7 +21,9 @@ class App extends Component {
     this.state = {
       input: "",
       imageUrl: "",
-      box: {}
+      box: {},
+      route: "signin",
+      isSignedIn: false
     }
   }
 
@@ -53,8 +57,14 @@ class App extends Component {
       .catch(err => console.log({ err }));
   }
 
+  onRouteChange = (route) => {
+    let isSignedIn = false
+    if (route === "home") isSignedIn = true;
+    this.setState({ route, isSignedIn });
+  }
+
   render() {
-    const { imageUrl } = this.state;
+    const { imageUrl, route, isSignedIn } = this.state;
     const particleOptions = {
       particles: {
         number: {
@@ -67,15 +77,19 @@ class App extends Component {
       }
     };
     return (
-      <div >
+      <div className="App">
         <Particles className="particles"
           params={particleOptions}
         />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-        <FaceRecognition box={this.state.box} imageUrl={imageUrl} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        {route === "signin" && <Signin onRouteChange={this.onRouteChange} />}
+        {route === "register" && <Register onRouteChange={this.onRouteChange} />}
+        {route === "home" && <React.Fragment>
+          <Logo />
+          <Rank />
+          <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+          <FaceRecognition box={this.state.box} imageUrl={imageUrl} />
+        </React.Fragment>}
       </div>
     );
   }
