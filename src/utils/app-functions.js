f394ba32b1bcce9e675a050c410703b86241ae49
Gavin Robertson <gavinrob92@gmail.com>
2019-10-10 21:24:54 -0600

    updated variables

--- src/utils/app-functions.js
+++ src/utils/app-functions.js
@@ -1,5 +1,6 @@
+const { REACT_APP_SIGNIN_URL, REACT_APP_PROFILE_URL } = process.env;
 export const signin = (email, password, onUserRetrevial) =>
-  fetch("http://localhost:3000/signin", {
+  fetch(REACT_APP_SIGNIN_URL, {
     method: "post",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, password })
@@ -9,7 +10,7 @@ export const signin = (email, password, onUserRetrevial) =>
       if (data.userId && data.success === "true") {
         window.sessionStorage.setItem("token", data.token);
         //TODO: make this into a function since it is being useed in App.js also
-        fetch(`http://localhost:3000/profile/${data.userId}`, {
+        fetch(`${REACT_APP_PROFILE_URL}/${data.userId}`, {
           headers: {
             "Content-Type": "application/json",
             Authorization: data.token
